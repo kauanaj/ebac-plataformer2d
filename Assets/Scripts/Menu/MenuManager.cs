bca5c43a697481f461f5f8e9c557e7916644cd1c
Kauan Jesus <kauan.aj@outlook.com>
2024-02-20 21:17:01 -0300

    Create scene menu e animation buttons

---
+++ Assets/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MenuManager : MonoBehaviour
+{
+    public List<GameObject> buttons;
+
+    [Header("Animation")]
+    public float duration = .2f;
+    public float delay = .05f;
+    public Ease ease = Ease.OutBack;  //ajuste de animação
+
+    private void OnEnable()
+    {
+        HideAllButtons();
+        ShowButtons();
+    }
+
+    private void HideAllButtons()
+    {
+        foreach(var b in buttons)
+        {
+            b.transform.localScale = Vector3.zero;
+            b.SetActive(false);
+        }
+    }
+
+    private void ShowButtons()
+    {
+        for(int i = 0; i < buttons.Count; i++ )
+        {
+            var b = buttons[i];
+            b.SetActive(true);
+            b.transform.DOScale(1, duration).SetDelay(i * delay).SetEase(ease);
+        }
+    }
+}
