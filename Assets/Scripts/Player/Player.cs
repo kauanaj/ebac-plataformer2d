cd1cd3bfd715c839bbdc0e3452e9edee933f90a9
Kauan Jesus <kauan.aj@outlook.com>
2024-02-23 20:26:42 -0300

    implemante prototype player moviment Run

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -9,7 +9,11 @@ public class Player : MonoBehaviour
     public Vector2 friction = new Vector2(.1f,0);
 
     public float speed;
+    public float speedRun;
+
     public float forceJump;
+
+    private float _currentSpeed;
 
     public void Update()
     {
@@ -19,15 +23,24 @@ public class Player : MonoBehaviour
 
     private void HandleMoviment()
     {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            _currentSpeed = speedRun;
+        }
+        else
+        {
+            _currentSpeed = speed;
+        }
+
         if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(_currentSpeed, myRigidbody.velocity.y);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
-            myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+            myRigidbody.velocity = new Vector2(-_currentSpeed, myRigidbody.velocity.y);
         }
 
         if(myRigidbody.velocity.x > 0)
