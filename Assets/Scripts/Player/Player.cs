608b1376939524c922599630a89475c8a27801d9
Kauan Jesus <kauan.aj@outlook.com>
2024-02-23 20:13:51 -0300

    implemente player moviment jump

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,13 +6,20 @@ public class Player : MonoBehaviour
 {
     public Rigidbody2D myRigidbody;
 
-    public Vector2 velocity;
+    public Vector2 friction = new Vector2(.1f,0);
 
     public float speed;
+    public float forceJump;
 
     public void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow)) 
+        HandleJump();
+        HandleMoviment();
+    }
+
+    private void HandleMoviment()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
         {
             //myRigidbody.MovePosition(myRigidbody.position + velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(speed, myRigidbody.velocity.y);
@@ -21,6 +28,23 @@ public class Player : MonoBehaviour
         {
             //myRigidbody.MovePosition(myRigidbody.position - velocity * Time.deltaTime);
             myRigidbody.velocity = new Vector2(-speed, myRigidbody.velocity.y);
+        }
+
+        if(myRigidbody.velocity.x > 0)
+        {
+            myRigidbody.velocity -= friction;
+        }
+        else if (myRigidbody.velocity.x < 0)
+        {
+            myRigidbody.velocity += friction;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            myRigidbody.velocity = Vector2.up * forceJump;
         }
     }
 }
