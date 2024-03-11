dcf82b017b06bc7c00055e7f5c8024e6aee787d1
Kauan Jesus <kauan.aj@outlook.com>
2024-03-11 19:40:50 -0300

    add score in interface

--- Assets/Scripts/Items/ItemManager.cs
+++ Assets/Scripts/Items/ItemManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 using Ebac.Core.Singleton;
 
 public class ItemManager : Singleton<ItemManager>
 {
     public int coins;
+    public TextMeshProUGUI uiScore;
 
     private void Start()
     {
@@ -15,10 +17,17 @@ public class ItemManager : Singleton<ItemManager>
     private void Reset()
     {
         coins = 0;
+        uiScore.text = "x0";
     }
 
     public void AddCoins(int amount = 1)
     {
         coins += amount;
+        UpdateCoins();
+    }
+    
+    private void UpdateCoins()
+    {
+        uiScore.text = ("x "+coins.ToString());
     }
 }
